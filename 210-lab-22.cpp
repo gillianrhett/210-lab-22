fdc74068b335fc55eab59d3e252abf9b3528c3bb
gillianrhett <gillianrhett@gmail.com>
2026-03-14 18:44:30 -0700

    working on delete_pos

--- 210-lab-22.cpp
+++ 210-lab-22.cpp
@@ -8,7 +8,7 @@ _ Create a method delete_pos() that will delete a node, not by its value, but by
     The method should also cover the case where we want to delete the head or tail node.
 _ Create a method pop_front(), that will delete the head node.
 _ Create a method pop_back() that will delete the tail node.
-_ Update the current delete() method to a better name, delete_val(). Keep the functionality
+x Update the current delete() method to a better name, delete_val(). Keep the functionality
     in the method that will still delete the head node, even though the new pop_front() 
     method will also do that. This is fine to code both, for the purposes of class completeness.
 */
@@ -81,7 +81,7 @@ class DoublyLinkedList {
         temp->next = newNode;
     }
 
-    void delete_node(int value) {
+    void delete_val(int value) {
         if (!head) return; // Empty list
             Node* temp = head;
         while (temp && temp->data != value)
@@ -118,6 +118,45 @@ class DoublyLinkedList {
             current = current->prev;
         }
         cout << endl;
+    }
+
+    void pop_front() {
+        
+    }
+
+    void pop_back() {
+
+    }
+
+    void delete_pos(int pos) { // delete the item at index #pos; head is 0
+        if (pos < 0) {
+            cout << "Invalid index." << endl;
+            return;
+        }
+        if (!head) { return; } // the list is already empty
+        Node* current = head;
+        int i = 0;
+        if (pos == 0) { // special case: delete the head
+            pop_front();
+            return;
+        }
+        while (current && i < pos) {
+            current = current->next;
+        }
+        if (!current) { // we have reached the end of the list
+            if (i != pos) // the chosen position is greater than the last index
+                cout << "Invalid index." << endl;
+            else { // we are at the last item
+                pop_back();
+            }
+        }
+        else { // the item to delete is in the middle
+            Node* predecessor = current->prev;
+            Node* successor = current->next->next;
+            predecessor->next = successor;
+            successor->prev = predecessor;
+            delete current;
+        }
     }
 
     ~DoublyLinkedList() {
